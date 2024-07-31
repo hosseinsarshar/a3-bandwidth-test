a4eddac42f0b2300bb4aa9b282b17336b74db9c9
Hossein Sarshar <hosseins@google.com>
2024-07-31 15:41:21 -0400

    fix

--- a3-mega/vertex/nemo/job/job_template.sh
+++ a3-mega/vertex/nemo/job/job_template.sh
@@ -108,11 +108,16 @@ mkdir -p /tmp/index_mapping_dir
 
 export RANK=0
 export NODE_RANK=$RANK         
-export GPUS_PER_NODE=8   
-export NNODES=1
+export GPUS_PER_NODE=8
 export WORLD_SIZE=8
 export MASTER_PORT=2222
 # export MASTER_ADDR=localhost
+
+echo RANK:$RANK
+echo NODE_RANK:$NODE_RANK
+echo GPUS_PER_NODE:$GPUS_PER_NODE
+echo WORLD_SIZE:$WORLD_SIZE
+echo MASTER_PORT:$MASTER_PORT
 
 echo "Launching Torch distributed as node rank $NODE_RANK out of $NNODES nodes"
 for ((LOCAL_RANK=0; LOCAL_RANK <= $((GPUS_PER_NODE - 1)); LOCAL_RANK++)); do
