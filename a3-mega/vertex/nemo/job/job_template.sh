34468c888f2f1e39637990e225f18cb083a70631
Hossein Sarshar <hosseins@google.com>
2024-07-31 20:32:24 -0400

    removed model.data.data_prefix

--- a3-mega/vertex/nemo/job/job_template.sh
+++ a3-mega/vertex/nemo/job/job_template.sh
@@ -131,12 +131,12 @@ for ((LOCAL_RANK=0; LOCAL_RANK <= $((GPUS_PER_NODE - 1)); LOCAL_RANK++)); do
     --config-path="/workspace/a3-bandwidth-test/a3-mega/vertex/nemo/nemo-configs" \
     --config-name="llama2-7b" \
     +trainer.num_nodes="$NNODES" \
-    +model.data.data_prefix="[1.0,/ssd/.cache/wikipedia-tokenized-for-llama2]" \
     +exp_manager.explicit_log_dir="/tmp/nemo-experiments/results" \
     +model.data.index_mapping_dir="/tmp/index_mapping_dir" \
     +exp_manager.version="$JOB_IDENTIFIER" \
     +exp_manager.exp_dir="/tmp/exp" \
     > /tmp/logs/rank-$RANK.log 2>&1 &
+    # +model.data.data_prefix="[1.0,/ssd/.cache/wikipedia-tokenized-for-llama2]" \
     # ${workload_arguments[@]} \
 
     echo "Launched rank $RANK with PID $!"
@@ -191,3 +191,73 @@ echo "Pod on $(hostname --fqdn) is exiting"
 ##
 #
 
+W0000 00:00:1722455981.557457    1804 zone_info_source.cc:122] RAW: Falling back to critical America/Los_Angeles zoneinfo data
+[NeMo I 2024-07-31 19:59:50 megatron_gpt_model:1587] Pipeline model parallel rank: 0, Tensor model parallel rank: 0, Number of model parameters on device: 6.89e+09. Number of precise model parameters on device: 6888361984.
+[NeMo I 2024-07-31 19:59:50 megatron_gpt_model:1441] Building GPT datasets.
+[NeMo I 2024-07-31 19:59:50 utils:47] Let split_matrix = [(0, 0.9), (0.9, 0.98), (0.98, 1.0)]
+[NeMo I 2024-07-31 19:59:50 utils:47] Let mock = True, as both blend and blend_per_split are None
+[NeMo I 2024-07-31 19:59:50 utils:47] Building dataset splits with cls=MockGPTDataset, sizes=[512000, 65536, 51200], and config=GPTDatasetConfig(random_seed=1234, sequence_length=4096, blend=None, blend_per_split=None, split='90,8,2', split_matrix=[(0, 0.9), (0.9, 0.98), (0.98, 1.0)], num_dataset_builder_threads=1, path_to_cache='/tmp/index_mapping_dir', mmap_bin_files=True, mock=True, tokenizer=<nemo.collections.common.tokenizers.huggingface.auto_tokenizer.AutoTokenizer object at 0x791a44f351e0>, reset_position_ids=False, reset_attention_mask=False, eod_mask_loss=False, create_attention_mask=True, drop_last_partial_validation_sequence=True, add_extra_token_to_sequence=True)
+[NeMo I 2024-07-31 19:59:50 utils:47] Build and save the MockGPTDataset train indices
+[NeMo I 2024-07-31 19:59:50 utils:47] > total number of samples: 539577
+[NeMo I 2024-07-31 19:59:50 utils:47] > total number of epochs: 12
+[NeMo I 2024-07-31 19:59:51 utils:47] Build and save the MockGPTDataset valid indices
+[NeMo I 2024-07-31 19:59:51 utils:47] > total number of samples: 67626
+[NeMo I 2024-07-31 19:59:51 utils:47] > total number of epochs: 17
+[rank0]:[E ProcessGroupNCCL.cpp:564] [Rank 0] Some NCCL operations have failed or timed out. Due to the asynchronous nature of CUDA kernels, subsequent GPU operations might run on corrupted/incomplete data.
+[rank0]:[E ProcessGroupNCCL.cpp:570] [Rank 0] To avoid data inconsistency, we are taking the entire process down.
+[rank0]:[E ProcessGroupNCCL.cpp:1335] [PG 0 Rank 0] NCCL watchdog thread terminated with exception: NCCL error: internal error - please report this issue to the NCCL developers, NCCL version 2.21.5
+ncclInternalError: Internal check failed.
+Last error:
+    @     0x79139d98e78c  ncclNetPluginShim_test()
+Exception raised from checkForNCCLErrorsInternal at /opt/pytorch/pytorch/torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp:1695 (most recent call first):
+frame #0: c10::Error::Error(c10::SourceLocation, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >) + 0x99 (0x791b6d39bdc9 in /usr/local/lib/python3.10/dist-packages/torch/lib/libc10.so)
+frame #1: c10d::ProcessGroupNCCL::checkForNCCLErrorsInternal(std::vector<std::shared_ptr<c10d::NCCLComm>, std::allocator<std::shared_ptr<c10d::NCCLComm> > > const&) + 0x34d (0x791b0bdb193d in /usr/local/lib/python3.10/dist-packages/torch/lib/libtorch_cuda.so)
+frame #2: c10d::ProcessGroupNCCL::WorkNCCL::checkAndSetException() + 0x7b (0x791b0bdb1beb in /usr/local/lib/python3.10/dist-packages/torch/lib/libtorch_cuda.so)
+frame #3: c10d::ProcessGroupNCCL::watchdogHandler() + 0x219 (0x791b0bdb90a9 in /usr/local/lib/python3.10/dist-packages/torch/lib/libtorch_cuda.so)
+frame #4: c10d::ProcessGroupNCCL::ncclCommWatchdog() + 0x125 (0x791b0bdb9ed5 in /usr/local/lib/python3.10/dist-packages/torch/lib/libtorch_cuda.so)
+frame #5: <unknown function> + 0xdc253 (0x791b6cab0253 in /lib/x86_64-linux-gnu/libstdc++.so.6)
+frame #6: <unknown function> + 0x94ac3 (0x791b874b9ac3 in /lib/x86_64-linux-gnu/libc.so.6)
+frame #7: <unknown function> + 0x126850 (0x791b8754b850 in /lib/x86_64-linux-gnu/libc.so.6)
+
+terminate called after throwing an instance of 'c10::DistBackendError'
+  what():  [PG 0 Rank 0] NCCL watchdog thread terminated with exception: NCCL error: internal error - please report this issue to the NCCL developers, NCCL version 2.21.5
+ncclInternalError: Internal check failed.
+Last error:
+    @     0x79139d98e78c  ncclNetPluginShim_test()
+Exception raised from checkForNCCLErrorsInternal at /opt/pytorch/pytorch/torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp:1695 (most recent call first):
+frame #0: c10::Error::Error(c10::SourceLocation, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >) + 0x99 (0x791b6d39bdc9 in /usr/local/lib/python3.10/dist-packages/torch/lib/libc10.so)
+frame #1: c10d::ProcessGroupNCCL::checkForNCCLErrorsInternal(std::vector<std::shared_ptr<c10d::NCCLComm>, std::allocator<std::shared_ptr<c10d::NCCLComm> > > const&) + 0x34d (0x791b0bdb193d in /usr/local/lib/python3.10/dist-packages/torch/lib/libtorch_cuda.so)
+frame #2: c10d::ProcessGroupNCCL::WorkNCCL::checkAndSetException() + 0x7b (0x791b0bdb1beb in /usr/local/lib/python3.10/dist-packages/torch/lib/libtorch_cuda.so)
+frame #3: c10d::ProcessGroupNCCL::watchdogHandler() + 0x219 (0x791b0bdb90a9 in /usr/local/lib/python3.10/dist-packages/torch/lib/libtorch_cuda.so)
+frame #4: c10d::ProcessGroupNCCL::ncclCommWatchdog() + 0x125 (0x791b0bdb9ed5 in /usr/local/lib/python3.10/dist-packages/torch/lib/libtorch_cuda.so)
+frame #5: <unknown function> + 0xdc253 (0x791b6cab0253 in /lib/x86_64-linux-gnu/libstdc++.so.6)
+frame #6: <unknown function> + 0x94ac3 (0x791b874b9ac3 in /lib/x86_64-linux-gnu/libc.so.6)
+frame #7: <unknown function> + 0x126850 (0x791b8754b850 in /lib/x86_64-linux-gnu/libc.so.6)
+
+Exception raised from ncclCommWatchdog at /opt/pytorch/pytorch/torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp:1339 (most recent call first):
+frame #0: c10::Error::Error(c10::SourceLocation, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >) + 0x99 (0x791b6d39bdc9 in /usr/local/lib/python3.10/dist-packages/torch/lib/libc10.so)
+frame #1: <unknown function> + 0xf6f04e (0x791b0bde604e in /usr/local/lib/python3.10/dist-packages/torch/lib/libtorch_cuda.so)
+frame #2: <unknown function> + 0xca016a (0x791b0bb1716a in /usr/local/lib/python3.10/dist-packages/torch/lib/libtorch_cuda.so)
+frame #3: <unknown function> + 0xdc253 (0x791b6cab0253 in /lib/x86_64-linux-gnu/libstdc++.so.6)
+frame #4: <unknown function> + 0x94ac3 (0x791b874b9ac3 in /lib/x86_64-linux-gnu/libc.so.6)
+frame #5: <unknown function> + 0x126850 (0x791b8754b850 in /lib/x86_64-linux-gnu/libc.so.6)
+
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] *** Process received signal ***
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] Signal: Aborted (6)
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] Signal code:  (-6)
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [ 0] /lib/x86_64-linux-gnu/libc.so.6(+0x42520)[0x791b87467520]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [ 1] /lib/x86_64-linux-gnu/libc.so.6(pthread_kill+0x12c)[0x791b874bb9fc]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [ 2] /lib/x86_64-linux-gnu/libc.so.6(raise+0x16)[0x791b87467476]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [ 3] /lib/x86_64-linux-gnu/libc.so.6(abort+0xd3)[0x791b8744d7f3]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [ 4] /lib/x86_64-linux-gnu/libstdc++.so.6(+0xa2b9e)[0x791b6ca76b9e]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [ 5] /lib/x86_64-linux-gnu/libstdc++.so.6(+0xae20c)[0x791b6ca8220c]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [ 6] /lib/x86_64-linux-gnu/libstdc++.so.6(+0xae277)[0x791b6ca82277]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [ 7] /lib/x86_64-linux-gnu/libstdc++.so.6(+0xae1fe)[0x791b6ca821fe]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [ 8] /usr/local/lib/python3.10/dist-packages/torch/lib/libtorch_cuda.so(+0xca0219)[0x791b0bb17219]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [ 9] /lib/x86_64-linux-gnu/libstdc++.so.6(+0xdc253)[0x791b6cab0253]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [10] /lib/x86_64-linux-gnu/libc.so.6(+0x94ac3)[0x791b874b9ac3]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] [11] /lib/x86_64-linux-gnu/libc.so.6(+0x126850)[0x791b8754b850]
+[gke-a3-mega-asia-a3-mega-asia-node-po-c2a38dbc-bgzz:00572] *** End of error message ***
+The target application terminated. One or more process it created re-parented.
+Waiting for termination of re-parented processes.
+Use the `--wait` option to modify this behavior.
