f61a86d653220d2c57f6a2d86a98e8c2fcaf9191
Hossein Sarshar <hosseins@google.com>
2024-08-07 13:44:30 -0400

    network bandwidth results are added

--- a3-mega/vertex/storage/fio-test.sh
+++ a3-mega/vertex/storage/fio-test.sh
@@ -41,59 +41,9 @@ export path_to_test=/gcs/hosseins-vertex-test/sd/fio-test-$RANK
 ./fio-scan $path_to_test
 
 
-
-# # Wait for Torch processes (might be problematic if only one fails)
-# for PID in ${TORCH_PIDS[*]}; do
-#     echo "Waiting on Torch PID $PID"
-#     wait $PID
-# done
-# 
-# sleep 600
 echo "sleep infinity on NODE_RANK:$NODE_RANK"
 sleep infinity
 echo "Pod on $(hostname --fqdn) is exiting"
 
 # copy one of the yaml config files, like llama2... to the helm folder -> selected-congifuration.yaml
 
-
-# # filesize=16k read                                                                                                                                           
-#                                                                                                                                                               
-# | lat msec | bw MBps |   IOPS   | jobs |                                                                                                                      
-# | -------: | ------: | -------: | ---: |                                                                                                                      
-# |    299.3 |    13.4 |     3419 |   16 |                                                                                                                      
-#                                                                                                                                                               
-# # filesize=16k write                                                                                                                                          
-#                                                                                                                                                               
-# | lat msec | bw MBps |   IOPS   | jobs |                                                                                                                      
-# | -------: | ------: | -------: | ---: |                                                                                                                      
-# |     89.2 |    44.9 |    11483 |   16 |                                                                                                                      
-#                                                                                                                                                               
-#                                                                                                                                                               
-#                                                                                                                                                               
-# # filesize=1m read                                                                                                                                            
-#                                                                                                                                                               
-# | lat msec | bw MBps |   IOPS   | jobs |                                                                                                                      
-# | -------: | ------: | -------: | ---: |                                                                                                                      
-# |    117.0 |    34.2 |     8754 |   16 |                                                                                                                      
-#                                                                                                                                                               
-# # filesize=1m write                                                                                                                                           
-#                                                                                                                                                               
-# | lat msec | bw MBps |   IOPS   | jobs |                                                                                                                      
-# | -------: | ------: | -------: | ---: |                                                                                                                      
-# |     89.7 |    44.6 |    11414 |   16 |                                                                                                                      
-#                                                                                                                                                               
-#                                                                                                                                                               
-#                                                                                                                                                               
-# # filesize=1g read                                                                                                                                            
-#                                                                                                                                                               
-# | lat msec | bw MBps |   IOPS   | jobs |                                                                                                                      
-# | -------: | ------: | -------: | ---: |                                                                                                                      
-# |    115.9 |    34.5 |     8837 |   16 |                                                                                                                      
-#                                                                                                                                                               
-# # filesize=1g write                                                                                                                                           
-#                                                                                                                                                               
-# | lat msec | bw MBps |   IOPS   | jobs |                                                                                                                      
-# | -------: | ------: | -------: | ---: |                                                                                                                      
-# |    100.4 |    39.9 |    10198 |   16 |                                                                                                                      
-#                                                                                                                                                               
-# 
