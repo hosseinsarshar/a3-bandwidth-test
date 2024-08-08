187f128242384d7081d1a75a1085d114e9fd679b
Hossein Sarshar <hosseins@google.com>
2024-08-07 20:50:32 -0400

    a

--- a3-mega/vertex/storage/fio-test.sh
+++ a3-mega/vertex/storage/fio-test.sh
@@ -40,10 +40,10 @@ cd ml-engineering/storage
 chmod +x ./fio-scan
 
 export path_to_test=/gcs/hosseins-vertex-test/sd/fio-test-$RANK
+echo "Participating nodes: $NNODES"
 echo "Bandwidth test starts on RANK:$RANK to test PATH:$path_to_test"
 
 ./fio-scan $path_to_test
-
 
 echo "sleep infinity on NODE_RANK:$NODE_RANK"
 sleep infinity
